42d58c2d77a727c112d2bafcfaab6b25ede12bbd
Michael Awad <awad.michael@216.nl>
2018-05-17 17:00:45 +0200

    First commit

---
+++ routes/leaderRouter.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the leaders to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /leaders');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all leaders');
+});
+
+leaderRouter.route('/:leaderId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res, next) => {
+  let leaderId = req.params.leaderId
+  res.end(`Will send leader number ${leaderId} to you!`);
+});
+
+
+module.exports = leaderRouter;
---
+++ routes/promoRouter.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promotionRouter = express.Router();
+
+promotionRouter.use(bodyParser.json());
+
+promotionRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the promotions to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all promotions');
+});
+
+promotionRouter.route('/:promotionId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res, next) => {
+  let promotionId = req.params.promotionId
+  res.end(`Will send promotion number ${promotionId} to you!`);
+});
+
+
+module.exports = promotionRouter;
